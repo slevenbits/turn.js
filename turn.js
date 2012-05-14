50b4c1101dd441d571baa32889b08d0c08e70a00
Guilhem Lettron <guilhem@lettron.fr>
2012-05-14 22:44:27 +0200

    Revert "Update of license" Keep an MIT license for future developpement in our company
    This reverts commit 8db4c68a16aca1d82bd0eedd8ead5c187772e4b9.

--- turn.js
+++ turn.js
@@ -1,20 +1,10 @@
 /**
- * turn.js 3rd release
- * www.turnjs.com
+ * turn.js
+ * turnjs.com
+ * Release: 3
  *
- * Copyright (C) 2012, Emmanuel Garcia.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *
- * 2. Any redistribution, use, or modification is done solely for personal 
- * benefit and not for any commercial purpose or for monetary gain.
- * 
+ * Copyright (C) 2012 Emmanuel Garcia
+ * Released under MIT License
  **/
 
 (function($) {
